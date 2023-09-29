6bcc0f227d5e2e6b371835190ec0452468362ad7
JoseCorreaMorales <jose.correax@gmail.com>
2023-09-28 19:52:29 -0600

    filtering pokemon by type

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,29 @@
 const URL = "https://pokeapi.co/api/v2/pokemon/";
 
+const pokeType = document.querySelectorAll(".poke-type");
+
+const pokemonList = document.querySelector(".columns");
+
+//let columns = document.querySelector(".columns");
+
 for (let i = 1; i <= 12; i++) {
-    fetch(URL + i).then(res => res.json()).then(data => {
-        showPokemon(data);
-    });
-    
+  fetch(URL + i).then(res => res.json()).then(data => {
+    showPokemon(data);
+  });
+
 }
 
 
 
- function showPokemon (data)  { 
+function showPokemon(data) {
+  const div = document.createElement("div");
+  div.classList.add("column", "is-4-tablet", "is-3-desktop", "mb-6");
   //let types  = data.types.map(type => type.type.name);
-  let types  = data.types.map((type) => {
+  let types = data.types.map((type) => {
     return `<span class="tag is-${type.type.name}">${type.type.name}</span>`;
   });
   types = types.join("");
-  let stats  = data.stats.map(stat => stat.base_stat);
+  let stats = data.stats.map(stat => stat.base_stat);
 
   let abilities = data.abilities.map((ability) => {
     return `<a href="#">#${ability.ability.name}</a>`;
@@ -24,10 +32,11 @@ for (let i = 1; i <= 12; i++) {
 
 
   let firstMove = data.moves.shift();
-  
-    let columns = document.querySelector(".columns");
-    columns.innerHTML += `
-    <div class="column is-4-tablet is-3-desktop mb-6">
+
+  //let columns = document.querySelector(".columns");
+  //columns.innerHTML += `
+  div.innerHTML += `
+    
     <div class="card mr-6">
       <div class="card-image">
         <div class="card-image__id">#${data.id}</div>
@@ -63,47 +72,38 @@ for (let i = 1; i <= 12; i++) {
         </div>
       </div>
     </div>
-  </div>
-    
+  
     
     `;
+  pokemonList.append(div);
 }
 
-/* 
- <div class="card">
-          <div class="card-image">
-            <div class="card-image__id">#123</div>
-            <figure class="image"><!--is-4by3  -->
-              <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-left">
-                <span class="tag is-white">ID</span>
-              </div>
-              <div class="media-content is-centered">
-                <p class="title is-4">Pokemon name </p>
-                <!-- <p class="subtitle is-6 has-text-centered">@johnsmith</p> -->
-              </div>
-            </div>
+pokeType.forEach(btn => btn.addEventListener("click", (e) => {
+  const typeId = e.currentTarget.id;
+  pokemonList.innerHTML = "";
 
-            <div class="content">
-              <div class="tags are-medium">
-                <span class="tag is-fire">Fire</span>
-                <span class="tag is-ground">Ground</span>
-              </div>
+  for (let i = 1; i <= 12; i++) {
+    fetch(URL + i).then(res => res.json()).then(data => {
+      //showPokemon(data);
 
-              <div class="tags stats">
-                <span class="tag is-white">HP: 100</span>
-                <span class="tag is-white">ATK: 100</span>
-              </div>
+      const types = data.types.map((type) => type.type.name);
+      console.log(types);
 
-              <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
-            </div>
-          </div>
-        </div>
-*/
+      if (typeId === "all") {
+        showPokemon(data);
+      } else {
+        if (types.some((type) => type === typeId)) {
+          showPokemon(data);
+
+        }
+      }
+
+
+
+
+    });
+
+  }
+
+
+}));
